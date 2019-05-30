590e4da3b5c0d9cbc9f8bae2b0d0e39dc02bf7a0
ClaudioRotunno <20019916@studenti.uniupo.it>
2019-05-30 15:35:39 +0200

    homepage

--- slideshow.js
+++ slideshow.js
@@ -12,3 +12,21 @@ function carousel() {
     x[slideIndex-1].style.display = "block";
     setTimeout(carousel, 2000); // Change image every 2 seconds
 }
+
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+var modal = document.getElementById('id02');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
